ca6b704f563b58d3b56d417ccce3afcf89011d9b
TURBOMODS <94942621+TURBOHYPER@users.noreply.github.com>
2022-07-17 16:36:05 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,99 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//global apis
+global.APIs = {
+	zenz: 'https://zenzapi.xyz', 
+}
+//global apikeys
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9', //api keys from zenzi
+}
+
+//settings
+global.owner = ['916380260672',''] //owner number +916380260672
+global.wame = ['916380260672'] 
+global.pemilik = ['916380260672'] //owner number +916380260672
+global.premium = ['916380260672'] //premium number +916380260672
+global.pengguna = 'TURBOHYPER' //username
+global.footer = '𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯'
+global.botnma = '𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫' //bot name
+global.botname = '𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫'
+global.ownernma = '𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯' //owner name
+global.packname = '𝗧𝚯𝗫𝗜𝗖 ₪ 𝚫𝗟𝗘𝗫𝚫' //sticker package name
+global.author = '𝗧𝚯𝗫𝗜𝗖 𝗧𝗨𝗥𝗕𝚯' //sticker author name
+global.sessionName = 'session' //session name
+global.prefa = ['#','.','!','/',''] //prefix
+global.sp = '🔵' //sp
+global.turbotyping = false
+global.turboreadgroup = false
+global.turboreadall = false
+global.turborecord = false
+global.available = false
+global.unavailable = true
+global.limittot = 100,
+global.thumb = fs.readFileSync('./TurboMedia/alexa.jpg')
+global.reactmoji = '🇮🇳'
+global.imgjoinrpg = fs.readFileSync('./image/joinrpg.jpg')
+global.imgmining = fs.readFileSync('./image/mining.jpg')
+global.imgdevil = fs.readFileSync('./image/img/devil.jpg')
+global.imgslime = fs.readFileSync('./image/img/slime.jpg')
+global.imgdemon = fs.readFileSync('./image/img/demon.jpg')
+global.imggoblin = fs.readFileSync('./image/img/goblin.jpg')
+global.imgdemonking = fs.readFileSync('./image/img/demonking.jpg')
+global.imgbehemoth = fs.readFileSync('./image/img/behemoth.jpg')
+global.f1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.f2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.f4 = 'application/zip'
+global.f5 = 'application/pdf'
+global.f6 = 'application/vnd.android.package-archive'
+global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
+global.mess = {
+    success: 'Dᴏɴᴇ ✓',
+    admin: 'Tʜɪs Fᴇᴀᴛᴜʀᴇ Is Oɴʟʏ Fᴏʀ Aᴅᴍɪɴ!',
+    botAdmin: 'Bᴏᴛ Mᴜsᴛ Bᴇ Aᴅᴍɪɴ Fɪʀsᴛ!',
+    owner: 'Tʜɪs Fᴇᴀᴛᴜʀᴇ Is Oɴʟʏ Fᴏʀ Oᴡɴᴇʀ',
+    group: 'Fᴇᴀᴛᴜʀᴇ Usᴇᴅ Oɴʟʏ Fᴏʀ Gʀᴏᴜᴘs!',
+    grouponly: 'Gʀᴏᴜᴘ Oɴʟʏ Mᴏᴅᴇ Is Oɴ',
+    private: 'Fᴇᴀᴛᴜʀᴇs Usᴇᴅ Oɴʟʏ Fᴏʀ Pʀɪᴠᴀᴛᴇ Cʜᴀᴛ!',
+    bot: 'Tʜɪs Fᴇᴀᴛᴜʀᴇ Iɴ Oɴʟʏ Fᴏʀ Tʜᴇ Bᴏᴛ Nᴜᴍʙᴇʀ',
+    wait: 'Pʟᴇᴀsᴇ Wᴀɪᴛ A Mɪɴᴜᴛᴇ...',
+    endLimit: '*Yᴏᴜʀ Dᴀɪʟʏ Lɪᴍɪᴛ Hᴀs Exᴘɪʀᴇᴅ, Tʜᴇ Lɪᴍɪᴛ Wɪʟʟ Bᴇ Rᴇsᴇᴛ Eᴠᴇʀʏ 12 Hᴏᴜʀs',
+    noPetualang: 'Yᴏᴜ Aʀᴇ Nᴏᴛ Iɴ Rᴘɢ Dᴀᴛᴀʙᴀsᴇ Tʏᴘᴇ *#joinrpg* _Uʀ Nᴀᴍᴇ_
+}
+global.limitawal = {
+    premium: "Infinity", //premium user limit
+    free: 100000 //free user limit
+}
+global.rpg = {
+darahawal: 100,
+besiawal: 15,
+goldawal: 10,
+emeraldawal: 5,
+umpanawal: 5,
+potionawal: 1
+}
+global.thumb = fs.readFileSync('./TurboMedia/alexa.jpg')
+global.visoka = fs.readFileSync('./TurboMedia/menu.mp4')
+global.vid = fs.readFileSync('./TurboMedia/menu.mp4')
+
+//—————「 Set Random Image Menu 」—————//
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.mehk = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
+global.awog = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
+global.mohai = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text='
+global.mhehe = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text='
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
